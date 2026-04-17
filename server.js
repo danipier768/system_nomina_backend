6a2a531be27312b3b9fe737a019e0161b0dbd50f
danipier768 <157076112+danipier768@users.noreply.github.com>
2026-04-17 16:50:52 -0500

    Add PATCH method to CORS configuration

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin:'https://system-nomina-frontend-g5qk.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
   })
 );
